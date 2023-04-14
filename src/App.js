29a846ef96deda46417e10b703c145aca074b16a
landontally <landontally+1@gmail.com>
2023-04-14 14:46:56 -0400

    feat: added express.js, mongoose, and cors

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import CocktailList from './components/CocktailList';
-import CocktailDetails from './components/CocktailDetails';
 
 function App() {
   const [cocktails, setCocktails] = useState([]);
-  const [selectedCocktail, setSelectedCocktail] = useState(null);
 
   const onSearch = (searchTerm) => {
     console.log('Search term:', searchTerm);
@@ -15,19 +13,11 @@ function App() {
     // For example: setCocktails(fetchedCocktails);
   };
 
-  const onSelectCocktail = (cocktail) => {
-    setSelectedCocktail(cocktail);
-  };
-
   return (
     <div className="App">
       <Header />
       <SearchBar onSearch={onSearch} />
-      {selectedCocktail ? (
-        <CocktailDetails cocktail={selectedCocktail} />
-      ) : (
-        <CocktailList cocktails={cocktails} onSelect={onSelectCocktail} />
-      )}
+      <CocktailList cocktails={cocktails} />
     </div>
   );
 }
