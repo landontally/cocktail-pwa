0bdd6ec6e381a66804e619dcabac8046a15c56bc
landontally <landontally+1@gmail.com>
2023-04-17 11:03:52 -0400

    style: updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,27 @@
 import React, { useState } from 'react';
-import './SearchBar.css'; // Import the CSS file (if using plain CSS)
+import styles from './SearchBar.module.css';
 
 const SearchBar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
     onSearch(searchTerm);
   };
 
   return (
-    <div className="search-bar">
-      <form onSubmit={handleSubmit} className="search-form">
-        <input
-          type="text"
-          placeholder="Search by ingredient"
-          value={searchTerm}
-          onChange={handleInputChange}
-          className="search-input"
-        />
-        <button type="submit" className="search-button">
-          Search
-        </button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit} className={styles.searchBar}>
+      <input
+        type="text"
+        placeholder="Search for a cocktail..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className={styles.input}
+      />
+      <button type="submit" className={styles.button}>
+        Search
+      </button>
+    </form>
   );
 };
 
